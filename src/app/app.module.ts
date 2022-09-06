f48159d932a59a67b19d0794d5d79268ef0e728f
SchroederSimon <schroeder.ssimon@gmail.com>
2022-09-05 21:42:46 -0300

    empezando main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 
+import { NgImageSliderModule } from 'ng-image-slider';
+import { MainContentComponent } from './components/main-content/main-content.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    MainContentComponent
   ],
   imports: [
     BrowserModule,
+    NgImageSliderModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Directive, Injectable, OnInit } from '@angular/core';
 
 
 @Component({
@@ -6,7 +6,20 @@ import { Component, Input, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
+
 export class HomeComponent implements OnInit {
+  
+  imageObject = [{
+    image: '/assets/imagenes/grafico2.png',
+    thumbImage: '/assets/imagenes/grafico2.png',
+    title: 'How people think it is'
+}, {
+    image: '/assets/imagenes/grafico1.png',
+    thumbImage: '/assets/imagenes/grafico1.png',
+    title: 'How actually is'
+}];
+
 
   constructor() { 
   }
